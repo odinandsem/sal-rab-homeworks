eadb7c1eabfa291fc10a5b43c0ba371c4db6697b
TEST <odinandsem@gmail.com>
2023-04-12 14:03:08 +0300

    DONE2

--- task_2/solution.js
+++ task_2/solution.js
@@ -29,11 +29,13 @@ function calcDiscount(sum, min, discount) {
 
     // Задание №2.2. Рассчитать скидку
 
-    // создайте переменную discountSum
+    let discountSum;
 
-    // если productsSum больше или равно discountMinSum,
-    // то присвойте discountSum значение discountPart процентов от productsSum,
-    // иначе присвойте discountSum значение 0
+    if (productsSum >= discountMinSum) {
+        discountSum = (discountPart / 100) * productsSum;
+    } else {
+        discountSum = 0;
+    }
 
     // Конец решения задания №2.2.
 
