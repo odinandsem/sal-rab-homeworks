4532bde7dd1014db3618825e38a08a14b86f304a
TEST <odinandsem@gmail.com>
2023-04-12 13:55:47 +0300

    DONE1

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,16 +5,17 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
-    // создайте переменную shippingSum
+    let shippingSum;
 
-    // если productsSum равно 0,
-    // то shippingSum присвоить значение 0
+    if (productsSum === 0) {
+        shippingSum = 0;
 
-    // если productsSum Больше или равна freeShippingMinSum,
-    // то shippingSum присвоить значение 0
+    } else if (productsSum >= freeShippingMinSum) {
+        shippingSum = 0;
 
-    // если productsSum больше 0 и меньше freeShippingMinSum,
-    // то shippingSum присвоить значение shippingPrice
+    } else {
+        shippingSum = shippingPrice;
+    }
 
     // Конец решения задания №2.1.
 
